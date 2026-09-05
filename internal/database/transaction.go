deaa0b3d26fae81fa7e00f7ca98c9d07a6da4eac
hani <gim325901@gmail.com>
2026-09-05 12:03:25 +0900

    feat: 트랜잭션 제어 추가

---
+++ internal/database/transaction.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type TransactionManager struct {
+	db *gorm.DB
+}
+
+func NewTransactionManager(db *gorm.DB) *TransactionManager {
+	return &TransactionManager{
+		db: db,
+	}
+}
+
+func (tm *TransactionManager) WithTransaction(
+	ctx context.Context,
+	fn func(tx *gorm.DB) error,
+) error {
+	return tm.db.WithContext(ctx).Transaction(fn)
+}
