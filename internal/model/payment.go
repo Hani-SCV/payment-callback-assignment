948892cd1f21d2aade1c31903d3e72b929632cc6
hani <gim325901@gmail.com>
2026-09-14 10:27:44 +0900

    fix: 데이터 타입 수정 Decimal

--- internal/model/payment.go
+++ internal/model/payment.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
 
 const (
 	PaymentStatusPending   = "PENDING"
@@ -10,22 +14,22 @@ const (
 )
 
 type TossReturnRequest struct {
-	PaymentKey string `json:"paymentKey"`
-	OrderID    string `json:"orderId"`
-	Amount     int64  `json:"amount"`
+	PaymentKey string          `json:"paymentKey"`
+	OrderID    string          `json:"orderId"`
+	Amount     decimal.Decimal `json:"amount"`
 }
 
 type Payment struct {
-	ID                    uint       `gorm:"primaryKey"`
-	PublicID              string     `gorm:"column:public_id"`
-	OrderID               int64      `gorm:"column:order_id"`
-	Provider              string     `gorm:"column:provider"`
-	Status                string     `gorm:"column:status"`
-	Amount                int64      `gorm:"column:amount"`
-	Currency              string     `gorm:"column:currency"`
-	ExternalTransactionID *string    `gorm:"column:external_transaction_id"`
-	CancellationReason    *string    `gorm:"column:cancellation_reason"`
-	CompletedAt           *time.Time `gorm:"column:completed_at"`
-	CreatedAt             time.Time  `gorm:"column:created_at"`
-	UpdatedAt             time.Time  `gorm:"column:updated_at"`
+	ID                    uint            `gorm:"primaryKey"`
+	PublicID              string          `gorm:"column:public_id"`
+	OrderID               int64           `gorm:"column:order_id"`
+	Provider              string          `gorm:"column:provider"`
+	Status                string          `gorm:"column:status"`
+	Amount                decimal.Decimal `gorm:"column:amount"`
+	Currency              string          `gorm:"column:currency"`
+	ExternalTransactionID *string         `gorm:"column:external_transaction_id"`
+	CancellationReason    *string         `gorm:"column:cancellation_reason"`
+	CompletedAt           *time.Time      `gorm:"column:completed_at"`
+	CreatedAt             time.Time       `gorm:"column:created_at"`
+	UpdatedAt             time.Time       `gorm:"column:updated_at"`
 }
