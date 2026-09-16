320c2a165e712de12c3367a6d28c6e081802f5e5
hani <gim325901@gmail.com>
2026-09-16 23:27:08 +0900

    style: gofmt

--- internal/model/payment.go
+++ internal/model/payment.go
@@ -7,23 +7,23 @@ import (
 )
 
 const (
-    PaymentStatusPending   = "PENDING"
-    PaymentStatusCompleted = "COMPLETED"
-    PaymentStatusFailed    = "FAILED"
-    PaymentStatusCanceled  = "CANCELED"
+	PaymentStatusPending   = "PENDING"
+	PaymentStatusCompleted = "COMPLETED"
+	PaymentStatusFailed    = "FAILED"
+	PaymentStatusCanceled  = "CANCELED"
 )
 
 type Payment struct {
-    ID                    uint
-    PublicID              string
-    OrderID               int64
-    Provider              string
-    Status                string
-    Amount                decimal.Decimal
-    Currency              string
-    ExternalTransactionID *string
-    CancellationReason    *string
-    CompletedAt           *time.Time
-    CreatedAt             time.Time
-    UpdatedAt             time.Time
+	ID                    uint
+	PublicID              string
+	OrderID               int64
+	Provider              string
+	Status                string
+	Amount                decimal.Decimal
+	Currency              string
+	ExternalTransactionID *string
+	CancellationReason    *string
+	CompletedAt           *time.Time
+	CreatedAt             time.Time
+	UpdatedAt             time.Time
 }
